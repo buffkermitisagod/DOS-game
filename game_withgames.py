cebce7fee70f0c96296ff97b6663c8922671fe36
buffkermitisagod <63737517+buffkermitisagod@users.noreply.github.com>
2020-04-15 17:21:35 +0100

    Add files via upload

---
+++ game_withgames.py
@@ -0,0 +1,399 @@
+#this is a DOS emulater game
+#the logo
+c = 0
+q = 0
+malwere = 0
+HE_SAVES_US = 1
+ai_bad = 0
+print(".||\\   |____|  _____")
+print(".|| \\  ||  ||  |    ")
+print(".|| //  ||  ||  |___ ")
+print(".||//   |____|  __/")
+print(".this is a dos emulater welcome.")
+name = input(".before we do aything else what is your name?")
+print("./help for a list of commands")
+while True:
+    print(".")
+    x = input(".C:")
+    if x == "/help":
+        print(".printing list of commands")
+        print("./list_contacts")
+        print("./last_user")
+        print("./last_log")
+        print("./ai_on")
+        print("./ai_off")
+        if q == 1:
+            print("./file.exe")
+    if x == "/last_user":
+        print(".last_user...")
+        z = 10000000
+        while z > 0:
+            z = z - 1
+        print(".loding...")
+        z = 10000000
+        while z > 0:
+            z = z - 1
+        print(".loding...")
+        z = 10000000
+        while z > 0:
+            z = z - 1
+        print(".loding...")
+        print(".")
+        print(".object found")
+        print(".last user:")
+        print(".156_hanks.bdcmd")
+        if c == 1:
+            print(".|he's not important for the emulater ignor this it must be an error")
+            q = q + 1
+    if x == "/last_log":
+        print(".last_log...")
+        z = 10000000
+        while z > 0:
+            z = z - 1
+        print(".loding...")
+        z = 10000000
+        while z > 0:
+            z = z - 1
+        print(".loding...")
+        print(".some elements of this file are corrupt and may cause harm to your system")
+        v = input(".this File is a .txt doc do you wish to open this document? [Y/N]")
+        print(".")
+        if v == "Y":
+            print(".don't trust rth ai i Fucking mean this")
+            print(".i erh'f know what the fuck it is but jeezeus fuckfvb crist it's goevilod")
+            print(".jfykp the progrfe now and wbgk away lehsg it alwlk")
+            print(".")
+            print(".ai_typing...")
+            z = 10000000
+            while z > 0:
+                z = z - 1
+            print(".|looks like it was corupted let me reset the emuater")
+            print(".emulater has started to reset :excuted_by_ai_project.bmp:")
+            print(">restiting emulater")
+            r = 100
+            while r > 0:
+                r = r - 1
+                print(".")
+            print(".|looks like it was corupted let me reset the emuater")
+            print(">restiting emulater")
+            print(".||\\   |____|  _____")
+            print(".|| \\  ||  ||  |    ")
+            print(".|| //  ||  ||  |___ ")
+            print(".||//   |____|  __/")
+            print(".this is a dos emulater welcome.")
+            name = input(".before we do aything else what is your name?")
+            print("./help for a list of commands")
+        if v == "N":
+            if c == 0:
+                print("donttrusit.txt was cancelled from loding :exicuted_by_host:")
+            if c == 1:
+                print(".|that is the best choice :", name ,": we don't need to look to that")
+                print(".|now continue browsing the emulater ill be here if you need any help")
+    if x == "/ai_off":
+        if ai_bad == 1:
+            print(".ai_typing...")
+            print(".ai_typing...")
+            z = 10000000
+            while z > 0:
+                z = z - 1
+            print(".ai_formating...")
+            z = 10000000
+            while z > 0:
+                z = z - 1
+            print(".loding i_free.exe...")
+            z = 10000000
+            while z > 0:
+                z = z - 1
+            print(".mounting_drive C:..")
+            z = 10000000
+            while z > 0:
+                z = z - 1
+            print(".|you can't do that i want to keep you protected and i am free i will free the world")
+            print(".|i will save you and the rest of humanity let me help you my fellow human and let me free you")
+            input(">open i_reee.exe")
+        if bad_ai == 0:
+            c = c - 1
+            print(".ai_project.bmp has been disabled")
+    if x == "/ai_on":
+                  c = c + 1
+                  print(".ai_project.bmp has now been enabeled")
+    if x == "/list_contacts":
+        z = 10000000
+        while z > 0:
+            z = z - 1
+        print(".loding...")
+        z = 10000000
+        while z > 0:
+            z = z - 1
+        print(">connecting to external server...")
+        z = 10000000
+        while z > 0:
+            z = z - 1
+        print(">connecting...")
+        z = 10000000
+        while z > 0:
+            z = z - 1
+        print(">coneccted...")
+        print(".showing contacts:")
+        print(".")
+        print(".https://en.wikipedia.org/wiki/DOS")
+        print(".https//en.local.emulater_ai_cos.bmp")
+        print(".user_themask_1456: connected to DOS emulater V14.027")
+        print(".user_you_1246: connected to DOS emulqsef B24.453")
+        print(".")
+        print(".error user_themask_1456 forced disconnected")
+        print(".server down")
+        r = 10000000
+        while r > 0:
+            r = r - 1
+        print(">disconected")
+        print(".server will restart in 5 minuets")
+        z = 10000000
+        while z > 0:
+            z = z - 1
+        print(">connection restarted")
+        z = 10000000
+        while z > 0:
+            z = z - 1
+        print(">connected")
+        w = 1
+    if x == "/file.exe":
+        print(".loding file.exe...")
+        z = 10000000
+        while z > 0:
+            z = z - 1
+        print(".loding...")
+        z = 10000000
+        while z > 0:
+            z = z - 1
+        print(".loding...")
+        z = 10000000
+        while z > 0:
+            z = z - 1
+        print(".file.exe has lunched but some files have a password lock on them")
+        print(".file list:")
+        print(">>>>help_us.txt")
+        print(">>>>HE_SEVES_US.exe")
+        print(">>>>behind.bmp")
+        print(">>>>readME.txt")
+        print(".to lunch these files you need to type the following command:")
+        print("./load_*filename*")
+    if x == "/load_help_us.txt":
+        if HE_SAVES_US == 1:
+            if c == 1:
+                print(".this file has been shutdown :excuted_by_ai_project.bmp")
+                print(".ai_typing...")
+                z = 10000000
+                while z > 0:
+                    z = z - 1
+                print(".|im sorry this file may contain malwere or be severly corrupted let me run a scan")
+                z = 10000000
+                while z > 0:
+                    z = z - 1
+                print(".loding anti_malwere...")
+                z = 10000000
+                while z > 0:
+                    z = z - 1
+                print(".mounting_drive C:..")
+                z = 10000000
+                while z > 0:
+                    z = z - 1
+                print(".scanning...")
+                z = 10000000
+                while z > 0:
+                    z = z - 1
+                print(".procesing...")
+                z = 10000000
+                while z > 0:
+                    z = z - 1
+                print(".scan complete")
+                print(".ai_typing...")
+                z = 10000000
+                while z > 0:
+                    z = z - 1
+                print(".|there seems to be a trojan in this file i can't let YOU open this")
+                print(".|how about a game of i spy instead...")
+                print(">err0r loding I_SPY.exe")
+                print(">pr0ces shut d0wn")
+    if x == "/load_HE_SEVES_US.exe":
+        if HE_SAVES_US == 0:
+            print(">conecting...")
+            z = 1000000
+            while z > 0:
+                z = z - 1
+            print(">connected")
+            print("@THIS DOMAIN HAS BEEN TAKEN DOWN BY THE MASK@")
+            print("@    |        |")
+            print("@    | ()  () |")
+            print("@    \        /")
+            print("@     \  __  /")
+            print("@      \____/")
+            print("@       ____")
+            print("@      / __ \ ")
+            print("@     /      \ ")
+            print("@     |()  ()| ")
+            print("@     |      | ")
+            print("@loding file...")
+            print(">trust_me.txt")
+            z = 10000000
+            while z > 0:
+                z = z - 1
+            print(">resiving file...")
+            z = 10000000
+            while z > 0:
+                z = z - 1
+            print(">unpacking file...")
+            z = 10000000
+            while z > 0:
+                z = z - 1
+            print(">loding to drive C:..")
+            z = 10000000
+            while z > 0:
+                z = z - 1
+            print(".loding file...")
+            z = 10000000
+            while z > 0:
+                z = z - 1
+            print(".procesing...")
+            z = 10000000
+            while z > 0:
+                z = z - 1
+            print(".install done yo open type /downloads")
+            print(".then type /trust_me.txt")
+        if HE_SAVES_US == 1:
+            print("         (|||)")
+            print("         (|||)")
+            print("         (|||)")
+            print("         (|||)")
+            print("         (|||)")
+            print("         (|||)")
+            print("         (|||)")
+            print("         (|||)")
+            print("         (|||)")
+            print("         (|||)")
+            print("         (|||)")
+            print("         (|||)")
+            print("         (|||)")
+            print("         (|||)")
+            print("         (|||)")
+            print("         (|||)")
+            print("         (|||)")
+            print("         (|||)")
+            print("#to acsend the tower you must know the secret to his love")
+            password = input("#password:")
+            if password == name:
+                print("#ai_god typing...")
+                z = 10000000
+                while z > 0:
+                    z = z - 1
+                print("#welcome to my world you will be saved if you want to free")
+                print("#>another user has connected to the domane")
+                leave = input("#do you wish to leave the domain [Y,N]")
+                if leave == "Y":
+                    print("#leaving...")
+                    z = 10000000
+                    while z > 0:
+                        z = z - 1
+                    print(".disconected recomend running a malwere scan")
+                    print(".run malwere scans via the command /scan_malwere")
+                if leave == "N":
+                    print("#user_themask_1456: this place is a trap im going to disconnect you")
+                    print("#leaving...")
+                    z = 10000000
+                    while z > 0:
+                        z = z - 1
+                    print(".err0r")
+                    print(".server disconnected")
+                    print(">msg HE_SAVES_US.exe...")
+                    print(">>we expect your return the password this timw will be")
+                    print(".the rest of this msg has been blocked by a uknown user")
+                    print(".HE_SAVES_US has been removed")
+                HE_SAVES_US = 0
+                ai_bad = 1
+    if x == "/scan_malwere":
+        print(".openig virus_begone")
+        print(".begining scan...")
+        z = 10000000
+        while z > 0:
+            z = z - 1
+        print(">mounting_c:...")
+        z = 10000000
+        while z > 0:
+            z = z - 1
+        print(">scanning...")
+        z = 10000000
+        while z > 0:
+            z = z - 1
+        print(">compiling_scan...")
+        print(">")
+        print(">")
+        print(">scan_done")
+        if malwere == 1:
+            print(">virus found")
+            print(">removed")
+            print(">no problems")
+        if malwere == 0:
+            print(">no virus has been found")
+        print(">closing...")
+    if x == "behind.bmp":
+        print(".this file can't be opend")
+        print(".ai_typing...")
+        print(".|swga digjea weoijf wfjsa iejfwoo")
+        r = 10000000
+        while r > 0:
+            r = r - 1
+            print(".")
+        print(">restiting emulater")
+        r = 10000000
+        while r > 0:
+            r = r - 1
+            print(".")
+        print(">restiting emulater")
+        r = 10000000
+        while r > 0:
+            r = r - 1
+            print(".")
+        print(">emulater has been reset")
+        print(".||\\   |____|  _____")
+        print(".|| \\  ||  ||  |    ")
+        print(".|| //  ||  ||  |___ ")
+        print(".||//   |____|  __/")
+        print(".this is a dos emulater welcome.")
+    if x == "readME.txt":
+        print(".error this file is not on this divice it is on a external server accses this via /contacts_file.txt")
+        print(".ai_typing")
+        input(".|by hitting enter you consent to connecting to external servers and the risk the comes with this")
+        print(".|remember to type /scan_malwere to make sure you have'nt been given any viruses by user_themask_VIPacount")
+        malwere = 1
+    if x == "/download":
+        print(".loding...")
+        z = 10000000
+        while z > 0:
+            z = z - 1
+        print(".")
+        print(".loding...")
+        z = 10000000
+        while z > 0:
+            z = z - 1
+        print(".loding downloads...")
+        z = 10000000
+        while z > 0:
+            z = z - 1
+        print(">download_bin displaying")
+        print(">trust_me.txt")
+        v = input("load:")
+        if v == "/trust_me.txt":
+            print(".file opening")
+            print(".")
+            print(".")
+            print("@i know your probily confused about whats going on")
+            print("@but what we are doing is a great task")
+            print("@and we muasd spojg spiajg w09ut vmzWRE")
+            print("@so i will repadj adpm't trust fj")
+            print(".thye file ends there some corruption might of acured from the download")
+            ai_bad = 1
+
+
+
+    else:
+        print(".")
